d6d044cddc644f0ecf9a135613a806d50ae41ee4
Fabio Carvalho <fabio.carvalho242@outlook.com>
2025-01-09 09:58:40 -0300

    Adiciona módulo de cursos com roteamento e componente inicial

---
+++ src/app/cursos/cursos/cursos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  standalone: false,
+  
+  templateUrl: './cursos.component.html',
+  styleUrl: './cursos.component.scss'
+})
+export class CursosComponent {
+
+}
