6a31fae104012d015bd0149fc900e9e13a00ecb7
Fabio Carvalho <fabio.carvalho242@outlook.com>
2025-01-09 12:07:21 -0300

    Adiciona tabela de cursos com colunas de nome e categoria, e implementa modelo Curso

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Curso } from '../models/curso';
 
 @Component({
   selector: 'app-cursos',
   standalone: false,
-  
+
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
 export class CursosComponent {
+
+  cursos: Curso[] = [];
+  displayedColumns = ['nome', 'categoria'];
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
 
 }
